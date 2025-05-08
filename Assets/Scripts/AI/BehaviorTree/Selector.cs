58398b9a9987bb2204eb3f8fe50a30b34e0db65f
Brian Feng <brainfeng135@gmail.com>
2025-05-07 17:57:33 -0700

    implement selector

--- Assets/Scripts/AI/BehaviorTree/Selector.cs
+++ Assets/Scripts/AI/BehaviorTree/Selector.cs
@@ -4,11 +4,26 @@ public class Selector : InteriorNode
 {
     public override Result Run()
     {
-        return Result.FAILURE;
+        Result res = children[current_child].Run();
+        if (res == Result.SUCCESS)
+        {
+            return Result.SUCCESS;
+        }
+        if (res == Result.FAILURE)
+        {
+            current_child++;
+        }
+        if (current_child >= children.Count)
+        {
+            current_child = 0;
+            return Result.FAILURE;
+        }
+        return Result.IN_PROGRESS;
     }
 
     public Selector(IEnumerable<BehaviorTree> children) : base(children)
     {
+
     }
 
     public override BehaviorTree Copy()
